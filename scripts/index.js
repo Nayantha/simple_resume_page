6466a562baeeeb7369004b5c742f2c4839fcea29
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-23 14:10:40 +0530

    add scroll up button variable.

--- scripts/index.js
+++ scripts/index.js
@@ -2,3 +2,4 @@ const scroll_down_btn = document.getElementsByClassName("scroll-down-btn")[0]
 scroll_down_btn.addEventListener("click", ()=>{
     window.scrollTo(0, document.body.scrollHeight)
 })
+const scroll_up_btn = document.getElementsByClassName("scroll-up-btn")[0]
