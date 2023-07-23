c969659c46ceccc26d08f35fc64da4467792e179
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-23 14:11:38 +0530

    add on click listener to scroll up button.

--- scripts/index.js
+++ scripts/index.js
@@ -3,3 +3,6 @@ scroll_down_btn.addEventListener("click", ()=>{
     window.scrollTo(0, document.body.scrollHeight)
 })
 const scroll_up_btn = document.getElementsByClassName("scroll-up-btn")[0]
+scroll_up_btn.addEventListener("click", ()=>{
+    window.scrollTo(100,document.body.scrollHeight)
+})
