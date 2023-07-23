bc3291b9531e3a9a0c89697f544af166240395f8
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-23 14:13:54 +0530

    add more linear smooth scroll function.

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 const scroll_down_btn = document.getElementsByClassName("scroll-down-btn")[0]
 scroll_down_btn.addEventListener("click", ()=>{
-    window.scrollTo(0, document.body.scrollHeight)
+    window.scrollTo({ left: 0, top: document.body.scrollHeight, behavior: "smooth" });
 })
 const scroll_up_btn = document.getElementsByClassName("scroll-up-btn")[0]
 scroll_up_btn.addEventListener("click", ()=>{
