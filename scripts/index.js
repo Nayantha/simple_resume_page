e2fb5be6f974e9edf956b9ae01f8f585d8c1ef6a
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-23 14:08:47 +0530

    add on click listener.

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,4 @@
 const scroll_down_btn = document.getElementsByClassName("scroll-down-btn")[0]
+scroll_down_btn.addEventListener("click", ()=>{
+    window.scrollTo(0, document.body.scrollHeight)
+})
