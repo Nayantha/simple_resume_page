7a322b75c9d4697612e4868d054852b02cb13b64
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-24 10:03:49 +0530

    add function to act when scrolled.

--- scripts/index.js
+++ scripts/index.js
@@ -6,3 +6,10 @@ const scroll_up_btn = document.getElementsByClassName("scroll-up-btn")[0]
 scroll_up_btn.addEventListener("click", ()=>{
     window.scrollTo({ left: 0, top: 0, behavior: "smooth" });
 })
+function scrollFunction(){
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+        scroll_down_btn.style.display = "block";
+    } else {
+        scroll_down_btn.style.display = "none";
+    }
+}
