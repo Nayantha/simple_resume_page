46741112d98b02d44720e78bd068e10326fccae8
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-13 19:05:17 +0530

    reformat the code.

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,19 @@
 const scroll_down_btn = document.getElementsByClassName("scroll-down-btn")[0]
-scroll_down_btn.addEventListener("click", ()=>{
-    window.scrollTo({ left: 0, top: document.body.scrollHeight, behavior: "smooth" });
+scroll_down_btn.addEventListener("click", () => {
+    window.scrollTo({left: 0, top: document.body.scrollHeight, behavior: "smooth"});
     scroll_down_btn.style.display = "none";
 })
 const scroll_up_btn = document.getElementsByClassName("scroll-up-btn")[0]
-scroll_up_btn.addEventListener("click", ()=>{
-    window.scrollTo({ left: 0, top: 0, behavior: "smooth" });
+scroll_up_btn.addEventListener("click", () => {
+    window.scrollTo({left: 0, top: 0, behavior: "smooth"});
 })
-function scrollFunction(){
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         scroll_up_btn.style.display = "block";
     } else {
         scroll_up_btn.style.display = "none";
     }
 }
-window.onscroll = ()=> scrollFunction()
+
+window.onscroll = () => scrollFunction()
